b631c1f6f39980b9364c6557c2e60faaea307a6b
bigmasterd <bigmasterdenis@gmail.com>
2022-09-18 21:56:36 +0200

    debug

--- src/build.sh
+++ src/build.sh
@@ -2,12 +2,12 @@ rm 6502
 rm *.o
 
 #compile libs but do not link
-gcc -c mem.c -o mem.o 
-gcc -c utils.c -o utils.o
-gcc -c test.c -o test.o
+gcc -g -c mem.c -o mem.o 
+gcc -g -c utils.c -o utils.o
+gcc -g -c test.c -o test.o
 
 #compile main and link libs
-gcc 6502.c -o 6502 mem.o utils.o test.o
+gcc -g 6502.c -o 6502 mem.o utils.o test.o
 
 #run if path to binary was given
 if [ $# -gt 0 ]; then
